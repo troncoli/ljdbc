8bea2de07c82c679f92c9e708bbc96ee445d44e6
Cyril <effarek@gmail.com>
2019-11-06 19:31:21 +0100

    CREATE Projectiles (Spawn, Movement, Collision, Death)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,29 +2,73 @@ extends KinematicBody2D
 
 export (int) var speed = 300
 
+var Projectile = preload("res://Objects/projectile-fireball.tscn")
 var velocity = Vector2()
+
+#shoot
+export (float) var shootingCooldown = 1.0
+var shootingTimer = shootingCooldown
+var shooting = false
+var lastPressed = 'ui_up'
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_key_pressed(KEY_K):
-		$AnimatedSprite.play("attack")
+	if Input.is_key_pressed(KEY_K) and !shooting: 
+		shoot()
 	else:
 		if Input.is_action_pressed('ui_right'):
 			velocity.x += 1
 			$AnimatedSprite.flip_h = false
+			lastPressed = 'ui_right'
 		if Input.is_action_pressed('ui_left'):
 			velocity.x -= 1
 			$AnimatedSprite.flip_h = true
+			lastPressed = 'ui_left'
 		if Input.is_action_pressed('ui_down'):
 			velocity.y += 1
+			lastPressed = 'ui_down'
 		if Input.is_action_pressed('ui_up'):
 			velocity.y -= 1
+			lastPressed = 'ui_up'
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
 			$AnimatedSprite.play("walking")
+		elif shooting and shootingTimer > 0.5:
+			$AnimatedSprite.play("attack")
 		else:
 			$AnimatedSprite.play("idle")
 
+func shoot():
+	shooting = true
+	$AnimatedSprite.play("attack")
+	
+	var p = Projectile.instance()
+	var spawnPos = position
+	var spawnRot = rotation
+	
+	if lastPressed == 'ui_right':
+		spawnPos += Vector2.RIGHT * 60
+		spawnRot = 0
+	elif lastPressed == 'ui_left':
+		spawnPos += Vector2.LEFT * 60
+		spawnRot = PI
+	elif lastPressed == 'ui_down':
+		spawnPos += Vector2.DOWN * 80
+		spawnRot = PI/2
+	elif lastPressed == 'ui_up':
+		spawnPos += Vector2.UP * 60
+		spawnRot = -PI/2
+	
+	p.start(spawnPos, spawnRot)
+	get_parent().add_child(p)
+
 func _physics_process(delta):
 	get_input()
+	
+	if shooting:
+		shootingTimer -= delta
+		if shootingTimer <= 0.0:
+			shooting = false
+			shootingTimer = shootingCooldown
+			
 	velocity = move_and_slide(velocity)
---
+++ Scripts/projectile-fireball.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+var speed = 500
+var velocity = Vector2()
+
+func start(pos, dir):
+	rotation = dir
+	position = pos
+	velocity = Vector2(speed, 0).rotated(rotation)
+	
+	if velocity.x > 0:
+		$AnimatedSprite.rotation = -PI/2
+	elif velocity.x < 0:
+		$AnimatedSprite.rotation = -PI/2
+	elif velocity.y > 0:
+		$AnimatedSprite.rotation = PI
+	elif velocity.y < 0:
+		$AnimatedSprite.rotation = 0
+
+func _physics_process(delta):
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		
+		queue_free()
